1f0cbba42f3044133358fe87b18fce54dec0007d
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2023-12-25 02:02:54 -0500

    Port to 1.20.2
    Fix block/item tags not being loaded due to typos
    Fix missing translation

--- src/main/java/com/github/creoii/survivality/mixin/block/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/com/github/creoii/survivality/mixin/block/AbstractFurnaceBlockEntityMixin.java
@@ -6,7 +6,7 @@ import com.github.creoii.survivality.util.SurvivalityTags;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,12 +17,12 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class AbstractFurnaceBlockEntityMixin {
-    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/AbstractFurnaceBlockEntity;canAcceptRecipeOutput(Lnet/minecraft/registry/DynamicRegistryManager;Lnet/minecraft/recipe/Recipe;Lnet/minecraft/util/collection/DefaultedList;I)Z", ordinal = 1, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT)
-    private static void survivality_explodeTntFuel(World world, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity blockEntity, CallbackInfo ci, boolean bl, boolean bl2, ItemStack itemStack, boolean bl3, boolean bl4, Recipe<?> recipe, int i) {
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/AbstractFurnaceBlockEntity;canAcceptRecipeOutput(Lnet/minecraft/registry/DynamicRegistryManager;Lnet/minecraft/recipe/RecipeEntry;Lnet/minecraft/util/collection/DefaultedList;I)Z", ordinal = 1, shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private static void survivality_explodeTntFuel(World world, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity blockEntity, CallbackInfo ci, boolean bl, boolean bl2, ItemStack itemStack, boolean bl3, boolean bl4, RecipeEntry recipeEntry, int i) {
         boolean value = !Survivality.CONFIG_AVAILABLE || ModMenuIntegration.CONFIG.explosiveFuelExplosionChance.floatValue() != 0f;
-        if (value && blockEntity.cookTime % 20 == 0 && world.random.nextFloat() <= ModMenuIntegration.CONFIG.explosiveFuelExplosionChance.floatValue() && itemStack.isIn(SurvivalityTags.EXPLODING_FUELS)) {
+        if (!world.isClient && value && blockEntity.cookTime % 20 == 0 && world.random.nextFloat() <= ModMenuIntegration.CONFIG.explosiveFuelExplosionChance.floatValue() && itemStack.isIn(SurvivalityTags.EXPLODING_FUELS)) {
             blockEntity.inventory.clear();
-            world.createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 4f, World.ExplosionSourceType.BLOCK);
+            world.createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 3.5f, World.ExplosionSourceType.TNT);
         }
     }
 }
